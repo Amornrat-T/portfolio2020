3f1398db0a38f5c8b55f4e39f3642b25fd4cbe6c
amornrat <amornrat@amornrats-MacBook-Air.local>
2020-11-10 15:46:17 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+function show() {
+    document.querySelector('.menu-btn').classList.toggle ('open')
+    document.querySelector('.logo').classList.toggle ('open')
+    document.querySelector('.slidebar').classList.toggle ('open')
+}
